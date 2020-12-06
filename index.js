313f07f1c0f8be02bc14fe3332452fcb304122c5
marcoshenriquemaia <henriquemarcosmaia@gmail.com>
2020-12-06 02:39:30 -0300

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+import dictionary from "./src/modules/dic.js"
+
+const $buttonProcess = document.querySelector(".button-process")
+
+$buttonProcess.addEventListener("click", () => {
+  const $input = document.querySelector(".input")
+  const $values = document.querySelector(".values")
+  const $startValue = document.querySelector(".start-index")
+  const $jump = document.querySelector(".jump")
+  const $column = document.querySelector(".column")
+  const $output = document.querySelector(".output")
+
+  const value = $values.value
+  const startIndex = parseInt($startValue.value)
+  const jump = parseInt($jump.value)
+  const column = $column.value
+  const input = $input.value
+  const output = $output.value
+  const arrayValues = value.split('\n')
+
+  let count = 0
+
+  const array = input?.split('\n')
+
+
+  for (let i = startIndex; i < array.length; i += jump) {
+    const mob = array[i]
+    const indexOfAtribute = dictionary.indexOf(column.toLowerCase())
+    const atribute = mob.split(',')
+
+    atribute[indexOfAtribute] = arrayValues[count]
+    
+    if (!mob) continue
+    
+    array[i] = atribute.join(',')
+    count++
+  }
+
+  $output.value = array.join('\n')
+  console.log(array)
+});
